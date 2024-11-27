4cafefb4ecb3534a36692af8804522f1d8d299c1
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-11-27 20:43:54 +0100

    Add files via upload

---
+++ src/ofxProjectM.h
@@ -0,0 +1,20 @@
+#pragma once
+#include "ofMain.h"
+#include "../libs/projectM/include/projectM-4/projectM.h"
+#include "../libs/projectM/include/projectM-4/playlist.h"
+
+class ofxProjectM {
+	public: ~ofxProjectM();
+	void load();
+	void render(int x, int y);
+	void render(int x, int y, int a, int b);
+	void audio(float* buffer);
+	void nextPreset();
+	char* getPresetName();
+	ofTexture getTexture();
+	int getMaxSamples();
+	projectm_handle projectMHandle;
+	projectm_playlist_handle projectMPlaylistHandle;
+	ofFbo fbo;
+	int windowWidth, windowHeight;
+};
